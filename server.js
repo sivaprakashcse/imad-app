db9b3c6031df2b0604435807d78900532ed563ce
U-SIVAPRAKASH-PC\siva prasad <sivaprakashcb@gmail.com>
2017-08-13 15:50:21 +0530

    Done server templating

--- server.js
+++ server.js
@@ -5,8 +5,126 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+	'article-one': {
+		title: 'Article One | Sivaprakash Veluthambi',
+		heading: 'Article One',
+		date: 'Aug 13, 2017',
+		content: `
+			<p>
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+			</p>
+			<p>
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+			</p>
+			<p>
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+				This is my first article. This is my first article. This is my first article.
+			</p>
+		`
+	},
+	'article-two': {
+		title: 'Article Two | Sivaprakash Veluthambi',
+		heading: 'Article Two',
+		date: 'Aug 13, 2017',
+		content: `
+			<p>
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+			</p>
+			<p>
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+			</p>
+			<p>
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+				This is my second article. This is my second article. This is my second article.
+			</p>
+		`
+	},
+	'article-three': {
+		title: 'Article Three | Sivaprakash Veluthambi',
+		heading: 'Article Three',
+		date: 'Aug 13, 2017',
+		content: `
+			<p>
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+			</p>
+			<p>
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+			</p>
+			<p>
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+				This is my third article. This is my third article. This is my third article.
+			</p>
+		`
+	}
+};
+
+function createTemplate (data) {
+	var title = data.title;
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+
+	var htmlTemplate = `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>${title}</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8" />
+			<link rel="stylesheet" type="text/css" href="/ui/style.css">
+		</head>
+		<body>
+			<div class="container">
+				<div>
+					<a href="/">Home</a>
+				</div>
+				<hr/>
+				<h3>${heading}</h3>
+				<div>
+					${date}
+				</div>
+				<div>
+					${content}
+				</div>
+			</div>
+		</body>
+		</html>
+	`;
+
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -21,7 +139,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
