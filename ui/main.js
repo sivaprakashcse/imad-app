bed421f81715d1fec774c528778165cfb50a748e
U-SIVAPRAKASH-PC\siva prasad <sivaprakashcb@gmail.com>
2017-08-14 17:14:11 +0530

    AJAX requests and API Calls done

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,58 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
+/*//Test button counter
+var button = document.getElementById('counter');
+var counter = 0;
 
-element.innerHTML = 'New Value';
+button.onclick = function () {
+	counter += 1;
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+};*/
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
+//Click Counter
+var button = document.getElementById('counter');
 
-function moveRight () {
-	marginLeft = marginLeft + 1;
-	img.style.marginLeft = marginLeft + 'px';
-}
+button.onclick = function () {
+	var request = new XMLHttpRequest();
 
-img.onclick = function () {
-	var interval = setInterval(moveRight, 50);
+	request.onreadystatechange = function () {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			if(request.status === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+	};
+
+	request.open('GET','http://localhost:8080/counter',true);
+	request.send(null);
+};
+
+//Submit name
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function () {
+	var request = new XMLHttpRequest();
+
+	request.onreadystatechange = function () {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			if(request.status === 200) {
+				var names = request.responseText;
+				names = JSON.parse(names);
+				var list = '';
+				for(var i=0; i< names.length; i++) {
+					list += '<li>' + names[i] + '</li>';
+				}
+				var ul = document.getElementById('namelist');
+				ul.innerHTML = list;
+			}
+		}
+	};
+
+	var nameInput = document.getElementById('name');
+	var name = nameInput.value;
+	request.open('GET','http://localhost:8080/submit-name?name=' + name,true);
+	request.send(null);
 };
