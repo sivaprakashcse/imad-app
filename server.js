39ab48aa16a4752df2e98bd05addc5399b482748
U-SIVAPRAKASH-PC\siva prasad <sivaprakashcb@gmail.com>
2017-08-14 17:12:04 +0530

    counter and input submit API routes added

--- server.js
+++ server.js
@@ -122,6 +122,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter += 1;
+  res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function (req, res) {
+  var name = req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
